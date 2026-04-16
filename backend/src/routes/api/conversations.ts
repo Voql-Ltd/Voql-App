f660647ccb730532dfb451710a746d61220f6ade
victorgbonna <victorgbonna@gmail.com>
2026-04-16 01:17:18 +0100

    switched back to microservices

--- backend/src/routes/api/conversations.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getConversations } from '../../controllers/conversations/getConversations';
-import requireAuth from '../../middleware/requireAuth';
-
-const router = Router();
-
-router.get('/all', requireAuth, getConversations);
-
-export default router;
--- backend/src/utils/consolelog.ts
+++
@@ -1,6 +0,0 @@
-export default function consoleLog(data: any): void {
-    if (process.env.NODE_ENV === 'production') return;
-    console.log('development console');
-    console.log(data);
-    return;
-}
